2d43e72e4ff11e413e0ed61198f01d6e2badec20
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-08-01 08:07:21 -0300

    feat iniciando o projeto

---
+++ src/page/index.jsx
@@ -0,0 +1,19 @@
+
+
+function Home (){
+
+return(
+    <>
+   <input type="text" placeholder=" o que tenho para fazer .... ?" />
+   <button>Adicionar</button>
+   <ul>
+    <li>Comprar pão</li>
+    <li>Comprar abacate</li>
+    <li>Comprar livro</li>
+   </ul>
+    </>
+)
+
+}
+
+export default Home
