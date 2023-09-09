1c36a1aabf11d733948af26544484fa935d85199
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-09-09 11:55:15 -0300

    feat Styled Components

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
+import { Container, } from "./styles";
 
 function Home() {
 
@@ -12,16 +13,18 @@ function Home() {
     }
 
     function CliqueiNoBotao() {
-        setListe([... List, {id: uuidv4(), task }])
+        setListe([...List, { id: uuidv4(), task }])
     }
 
     return (
         <>
-            <input type="text" onChange={IpuntMudou} placeholder=" o que tenho para fazer .... ?" />
-            <button onClick={CliqueiNoBotao} >Adicionar</button>
-            <ul>
-                {List.map(Intems => (<li key={Intems.id}>{Intems.task}</li>))}
-            </ul>
+            <Container>
+                <input type="text" onChange={IpuntMudou} placeholder=" o que tenho para fazer .... ?" />
+                <button onClick={CliqueiNoBotao} >Adicionar</button>
+                <ul>
+                    {List.map(Intems => (<li key={Intems.id}>{Intems.task}</li>))}
+                </ul>
+            </Container>
         </>
     )
 
---
+++ src/page/styles.js
@@ -0,0 +1,5 @@
+import  Styled, { styled } from "styled-components";
+
+export const Container = styled.div`
+background: linear-gradient(90deg, #383838 0%, #000000 81.21%);
+`
