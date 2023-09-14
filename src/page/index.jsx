fd1d98d3b4475b87acf1953b2ab9020ce0a09f1d
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-09-14 15:37:54 -0300

    feat Icones personalizados no React

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-
-import { Container, TodoList, Input,Button } from "./styles";
+import { FcFullTrash, FcCheckmark } from "react-icons/fc";
+import { Container, TodoList, Input, Button } from "./styles";
 
 function Home() {
 
@@ -20,11 +20,11 @@ function Home() {
         <>
             <Container>
                 <TodoList>
-                <Input type="text" onChange={IpuntMudou} placeholder=" o que tenho para fazer .... ?" />
-                <Button onClick={CliqueiNoBotao} >Adicionar</Button>
-                <ul>
-                    {List.map(Intems => (<li key={Intems.id}>{Intems.task}</li>))}
-                </ul>
+                    <Input type="text" onChange={IpuntMudou} placeholder=" o que tenho para fazer .... ?" />
+                    <Button onClick={CliqueiNoBotao} >Adicionar</Button>
+                    <ul>
+                        {List.map(Intems => (<div> <FcCheckmark /> <li key={Intems.id}>{Intems.task}</li> <FcFullTrash /> </div>))}
+                    </ul>
                 </TodoList>
             </Container>
         </>
